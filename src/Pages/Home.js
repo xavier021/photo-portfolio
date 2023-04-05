adea78360471fb4559d99442ce4901e1c737a6c4
Xavier Santana <xaviersantana@Xaviers-MacBook-Pro.local>
2023-04-04 21:39:43 -0400

    Edited home page, draft

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
 import MainLayout from "../Components/MainLayout";
+import { Col,Row, Container, Image } from "react-bootstrap";
+import "./Home.css"
 
 function Home() {
   return (
     <MainLayout>
-      <h3>
+    <Container className="custom-container">
+        <Row className="custom-row">
+            <Col>
+                <h2 className="custom-name">Xavier Santana</h2>
+                <br/>
+                <h1 className="custom-name">PHOTOGRPAHER</h1>
+            </Col>
+            <Col className="custom-col">
+                <Image 
+                    src="/View/DSC05108-X.webp"
+                    fluid
+                />
+            </Col>
+        </Row>
+    </Container>
+      {/* <h3>
         HomeLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
         eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
         minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
@@ -113,7 +130,7 @@ function Home() {
         in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
         pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
         qui officia deserunt mollit anim id est laborum.
-      </h3>
+      </h3> */}
     </MainLayout>
   );
 }
