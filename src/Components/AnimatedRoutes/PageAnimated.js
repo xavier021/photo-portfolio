330ca24207869a5a984cf2808df2ca88274e49e9
Xavier Santana <xsantana001@gmail.com>
2023-05-06 15:27:36 -0400

    Compressed images

--- src/Components/AnimatedRoutes/PageAnimated.js
+++ src/Components/AnimatedRoutes/PageAnimated.js
@@ -14,7 +14,7 @@ export function PageAnimated({ children }) {
     <motion.div
       initial="hidden"
       animate="visible"
-    //   exit={{ opacity: 0, transition: { duration: 1 } }}
+      // exit={{ opacity: 0, transition: { duration: 1 } }}
       variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
     >
       {children}
